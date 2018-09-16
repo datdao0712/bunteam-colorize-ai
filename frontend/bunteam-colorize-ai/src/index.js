26f7669bee4e16472eb73be215a65895b69b57ee
dat.dao <dat.dao@tiki.vn>
2018-09-16 16:36:20 +0700

    Add ant design lib

--- frontend/bunteam-colorize-ai/src/index.js
+++ frontend/bunteam-colorize-ai/src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
-import { HashRouter } from 'react-router-dom'
+import { ConnectedRouter } from 'connected-react-router'
+import { Provider } from 'react-redux'
+import store, { history } from './store'
 ReactDOM.render(
-    <HashRouter>
-        <App />
-    </HashRouter>
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+                <App />
+        </ConnectedRouter>
+    </Provider>
 , document.getElementById("root"));
